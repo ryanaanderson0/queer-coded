81a3f1543af0d98a0539462643eb369cbae88e6c
Ryan Anderson <ryananderson@Ryans-MBP.home>
2023-04-04 19:30:58 -0700

    hero styles

--- pages/components/hero/hero.js
+++ pages/components/hero/hero.js
@@ -1,7 +1,25 @@
+import Image from "next/image";
+
 export default function Hero() {
   return (
-    <div className="min-h-screen flex justify-center items-center bg-[url('/rainbow.jpeg')] bg-no-repeat bg-cover">
-      <p>Hero Test</p>
+    <div className="min-h-fit grid grid-cols-2 gap-4 bg-[url('/rainbow.jpeg')] bg-no-repeat bg-cover">
+      <div className="flex flex-col items-center p-16">
+        <h1 className="antialiased font-semibold text-7xl">
+          Queer centric coding groups and resources
+        </h1>
+        <p className="text-slate-500">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+        <button className="rounded-full bg-red-400 p-3" bg->
+          Join The Community
+        </button>
+      </div>
+      <div className="flex justify-center items-center p-16">
+        <Image src="test.svg" width={500} height={500} />
+      </div>
     </div>
   );
 }
